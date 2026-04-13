d56e6a358bf74d628fa232539a36a60c9e7796d9
aruninom <arunkumar@inomerp.in>
2026-04-13 10:59:50 +0530

    Add Odoo training catalog module for events and portfolio

---
+++ inom_training_catalog/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "INOM Training Catalog",
+    "summary": "Public training catalog with provider/content filters and conditional visibility",
+    "version": "19.0.1.0.0",
+    "category": "Website/Event",
+    "author": "INOM",
+    "license": "LGPL-3",
+    "depends": ["website", "event", "website_event"],
+    "data": [
+        "views/event_event_views.xml",
+        "views/training_catalog_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
