7375af206baa4542dd7594c89a3bf3c6c879b0ac
aruninom <arunkumar@inomerp.in>
2026-04-30 14:54:11 +0530

    Fix Excel export crash when report level is string

---
+++ base_accounting_kit_excel_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Base Accounting Kit - Financial Report Excel',
+    'version': '18.0.1.0.0',
+    'summary': 'Adds Excel export button on Financial Report wizard',
+    'category': 'Accounting',
+    'depends': ['base_accounting_kit'],
+    'data': [
+        'views/financial_report_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
